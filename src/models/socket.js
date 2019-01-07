4c078b3687d19c88a4de15b2fd67a704bb94cc1f
韩文博 <9476400@qq.com>
2019-01-07 13:29:35 +0800

    fixed

--- src/models/socket.js
+++ src/models/socket.js
@@ -1,6 +1,7 @@
 import * as service from "@/services/socket";
 // 全局铃声
 var audio;
+
 export default {
     namespace: "socket",
     state: {},
@@ -8,24 +9,26 @@ export default {
     reducers: {},
     subscriptions: {
         socket({ dispatch }) {
-            // 注册全局铃声
-            window.onload = function() {
-                // 解决第一时间有通知进来无法播放而报错
-                setTimeout(() => {
-                    audio = window.document.createElement("audio");
-                    audio.src = require("@/assets/mp3/order-notice.mp3");
-                    audio.load();
-                    audio.pause();
-                }, 0);
-            };
             // 验证登陆
             const token = JSON.parse(localStorage.getItem("token"));
             if (token) {
+                // 注册全局铃声
+                window.onload = function() {
+                    setTimeout(() => {
+                        audio = window.document.createElement("audio");
+                        audio.style = 'display:none'
+                        window.document.body.appendChild(audio);
+                        audio.src = require("@/assets/mp3/order-notice.mp3");
+                        audio.load();
+                        audio.pause();
+                    }, 500);
+                };
                 return service.listen(data => {
                     switch (data.action) {
                         case "admin.order.pay":
                             try {
                                 if (audio) {
+                                    console.log(audio)
                                     var isPlaying = audio.currentTime > 0 && !audio.paused && !audio.ended
                                         && audio.readyState > 2;
 
@@ -40,6 +43,7 @@ export default {
                                                 }
                                             };
                                             catchChromeException(audio.play());
+                                            audio.play()
                                         }
                                     }
                                 }
