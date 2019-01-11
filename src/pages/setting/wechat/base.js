eecf330ba16bae4e7d02fc09d3ed12272d75002b
韩文博 <9476400@qq.com>
2019-01-11 10:40:13 +0800

    fix: 解决用户反馈的微信配置不上传证书无法保存的bug

--- src/pages/setting/wechat/base.js
+++ src/pages/setting/wechat/base.js
@@ -104,7 +104,7 @@ class Payment extends Component {
         this.props.form.validateFieldsAndScroll((err, values) => {
             if (!err) {
                 const { apiclientCert, apiclientKey } = this.state;
-                const { app_id, app_secret, status, mini_app_id,mini_app_secret, mch_id, key ,callback_domain} = values;
+                const { app_id, app_secret, status, mini_app_id, mini_app_secret, mch_id, key, callback_domain } = values;
                 const { dispatch } = this.props;
                 dispatch({
                     type: "setting/edit",
@@ -118,11 +118,11 @@ class Payment extends Component {
                             mch_id,
                             key,
                             apiclient_cert:
-                                typeof apiclientCert[0]["path"] !== "undefined"
+                                Array.isArray(apiclientCert) && apiclientCert.length > 0 && typeof apiclientCert[0]["path"] !== "undefined"
                                     ? apiclientCert[0].path
                                     : "",
                             apiclient_key:
-                                typeof apiclientKey[0]["path"] !== "undefined"
+                                Array.isArray(apiclientKey) && apiclientKey.length > 0 && typeof apiclientKey[0]["path"] !== "undefined"
                                     ? apiclientKey[0].path
                                     : "",
                             callback_domain
@@ -133,10 +133,12 @@ class Payment extends Component {
                         if (response.code === 0) {
                             message.success("修改成功");
                         } else {
-                            message.warn(response.msg);
+                            message.error(response.msg);
                         }
                     }
                 });
+            } else {
+                console.log(err);
             }
         });
     };
@@ -150,155 +152,155 @@ class Payment extends Component {
         const token = JSON.parse(localStorage.getItem("token"));
         const headers = { "Access-Token": token.accessToken };
         return (
-                <Card bordered={false}>
-                    <Spin size="large" spinning={settingInfoLoading}>
-                        <Alert
-                            message="注意：App手机登录、微信登录用户打通"
-                            description={
-                                <span>
+            <Card bordered={false}>
+                <Spin size="large" spinning={settingInfoLoading}>
+                    <Alert
+                        message="注意：App手机登录、微信登录用户打通"
+                        description={
+                            <span>
                                     如果您使用了App并且使用了手机登录和微信登录方式，请去配置微信开放平台绑定它们之间的关系，
                                     <a href={`https://www.fashop.cn/help/login-ways`} target="_blank">
                                         详情请见
                                     </a>
                                 </span>
-                            }
-                            type="info"
-                            showIcon
-                        />
-                        <Form
-                            onSubmit={this.handleSubmit}
-                            style={{
-                                marginTop: 24
-                            }}
+                        }
+                        type="info"
+                        showIcon
+                    />
+                    <Form
+                        onSubmit={this.handleSubmit}
+                        style={{
+                            marginTop: 24
+                        }}
+                    >
+                        <FormItem
+                            {...formItemLayout}
+                            label="微信商户ID"
+                            // extra="微信支付商户号，审核通过后，会发送到申请时的邮箱"
                         >
-                            <FormItem
-                                {...formItemLayout}
-                                label="微信商户ID"
-                                // extra="微信支付商户号，审核通过后，会发送到申请时的邮箱"
-                            >
-                                {getFieldDecorator("mch_id", {
-                                    rules: [
-                                        {
-                                            required: true,
-                                            message: "请输入"
-                                        }
-                                    ],
-                                    initialValue: mch_id
-                                })(<Input placeholder="请输入" />)}
-                            </FormItem>
-                            <FormItem {...formItemLayout} label="微信商户API密钥">
-                                {getFieldDecorator("key", {
-                                    rules: [
-                                        {
-                                            required: true,
-                                            message: "请输入"
-                                        }
-                                    ],
-                                    initialValue: key
-                                })(<Input placeholder="请输入" />)}
-                            </FormItem>
-                            <FormItem {...formItemLayout} label="小程序App Id">
-                                {getFieldDecorator("mini_app_id", {
-                                    initialValue: mini_app_id
-                                })(<Input placeholder="请输入" />)}
-                            </FormItem>
-                            <FormItem {...formItemLayout} label="小程序App Secret">
-                                {getFieldDecorator("mini_app_secret", {
-                                    initialValue: mini_app_secret
-                                })(<Input placeholder="请输入" />)}
-                            </FormItem>
-                            <FormItem {...formItemLayout} label="公众号App Id">
-                                {getFieldDecorator("app_id", {
-                                    initialValue: app_id
-                                })(<Input placeholder="请输入" />)}
-                            </FormItem>
-                            <FormItem {...formItemLayout} label="公众号App Secret">
-                                {getFieldDecorator("app_secret", {
-                                    initialValue: app_secret
-                                })(<Input placeholder="请输入" />)}
-                            </FormItem>
-                            <FormItem
-                                {...formItemLayout}
-                                label="CERT证书"
-                                extra="下载证书 cert.zip 中的 apiclient_cert.pem 文件，微信退款原路退回时所需"
-                            >
-                                {getFieldDecorator("apiclient_cert", {})(
-                                    <Upload
-                                        name={name}
-                                        action={action}
-                                        headers={headers}
-                                        onChange={e => {
-                                            this.onApiclientCertChange(e);
-                                        }}
-                                        fileList={apiclientCert}
-                                    >
-                                        <Button>
-                                            <Icon type="upload" /> 上传证书
-                                        </Button>
-                                        {Array.isArray(apiclientCert) && apiclientCert.length > 0 ? (
-                                            <span style={{ marginLeft: 10, color: "green" }}>
+                            {getFieldDecorator("mch_id", {
+                                rules: [
+                                    {
+                                        required: true,
+                                        message: "请输入"
+                                    }
+                                ],
+                                initialValue: mch_id
+                            })(<Input placeholder="请输入" />)}
+                        </FormItem>
+                        <FormItem {...formItemLayout} label="微信商户API密钥">
+                            {getFieldDecorator("key", {
+                                rules: [
+                                    {
+                                        required: true,
+                                        message: "请输入"
+                                    }
+                                ],
+                                initialValue: key
+                            })(<Input placeholder="请输入" />)}
+                        </FormItem>
+                        <FormItem {...formItemLayout} label="小程序App Id">
+                            {getFieldDecorator("mini_app_id", {
+                                initialValue: mini_app_id
+                            })(<Input placeholder="请输入" />)}
+                        </FormItem>
+                        <FormItem {...formItemLayout} label="小程序App Secret">
+                            {getFieldDecorator("mini_app_secret", {
+                                initialValue: mini_app_secret
+                            })(<Input placeholder="请输入" />)}
+                        </FormItem>
+                        <FormItem {...formItemLayout} label="公众号App Id">
+                            {getFieldDecorator("app_id", {
+                                initialValue: app_id
+                            })(<Input placeholder="请输入" />)}
+                        </FormItem>
+                        <FormItem {...formItemLayout} label="公众号App Secret">
+                            {getFieldDecorator("app_secret", {
+                                initialValue: app_secret
+                            })(<Input placeholder="请输入" />)}
+                        </FormItem>
+                        <FormItem
+                            {...formItemLayout}
+                            label="CERT证书"
+                            extra="下载证书 cert.zip 中的 apiclient_cert.pem 文件，微信退款原路退回时所需"
+                        >
+                            {getFieldDecorator("apiclient_cert", {})(
+                                <Upload
+                                    name={name}
+                                    action={action}
+                                    headers={headers}
+                                    onChange={e => {
+                                        this.onApiclientCertChange(e);
+                                    }}
+                                    fileList={apiclientCert}
+                                >
+                                    <Button>
+                                        <Icon type="upload" /> 上传证书
+                                    </Button>
+                                    {Array.isArray(apiclientCert) && apiclientCert.length > 0 ? (
+                                        <span style={{ marginLeft: 10, color: "green" }}>
                                                     已上传
                                                 </span>
-                                        ) : (
-                                            <span style={{ marginLeft: 10 }}>未上传</span>
-                                        )}
-                                    </Upload>
-                                )}
-                            </FormItem>
-                            <FormItem
-                                {...formItemLayout}
-                                label="KEY密钥文件"
-                                extra="下载证书 cert.zip 中的 apiclient_key.pem 文件，微信退款原路退回时所需"
-                            >
-                                {getFieldDecorator("apiclient_key", {})(
-                                    <Upload
-                                        name={name}
-                                        action={action}
-                                        headers={headers}
-                                        onChange={e => {
-                                            this.onApiclientKeyChange(e);
-                                        }}
-                                        fileList={apiclientKey}
-                                    >
-                                        <Button>
-                                            <Icon type="upload" /> 上传证书
-                                        </Button>
-                                        {Array.isArray(apiclientKey) && apiclientKey.length > 0 ? (
-                                            <span style={{ marginLeft: 10, color: "green" }}>
+                                    ) : (
+                                        <span style={{ marginLeft: 10 }}>未上传</span>
+                                    )}
+                                </Upload>
+                            )}
+                        </FormItem>
+                        <FormItem
+                            {...formItemLayout}
+                            label="KEY密钥文件"
+                            extra="下载证书 cert.zip 中的 apiclient_key.pem 文件，微信退款原路退回时所需"
+                        >
+                            {getFieldDecorator("apiclient_key", {})(
+                                <Upload
+                                    name={name}
+                                    action={action}
+                                    headers={headers}
+                                    onChange={e => {
+                                        this.onApiclientKeyChange(e);
+                                    }}
+                                    fileList={apiclientKey}
+                                >
+                                    <Button>
+                                        <Icon type="upload" /> 上传证书
+                                    </Button>
+                                    {Array.isArray(apiclientKey) && apiclientKey.length > 0 ? (
+                                        <span style={{ marginLeft: 10, color: "green" }}>
                                                     已上传
                                                 </span>
-                                        ) : (
-                                            <span style={{ marginLeft: 10 }}>未上传</span>
-                                        )}
-                                    </Upload>
-                                )}
-                            </FormItem>
-                            <FormItem {...formItemLayout} label="回调域名" extra="使用场景：微信支付异步通知、退款原路返回"
-                            >
-                                {getFieldDecorator("callback_domain", {
-                                    rules: [
-                                        {
-                                            required: true,
-                                            message: "请输入回调域名"
-                                        }
-                                    ],
-                                    initialValue: callback_domain
-                                })(<Input placeholder="请输入回调域名，如：https://www.fashop.cn" />)}
-                            </FormItem>
-                            <FormItem {...formItemLayout} label="支付开关">
-                                {getFieldDecorator("status", {
-                                    valuePropName: "checked",
-                                    initialValue: status === 1
-                                })(<Switch />)}
-                            </FormItem>
-                            <FormItem {...tailFormItemLayout}>
-                                <Button type="primary" htmlType="submit" loading={settingEditLoading}>
-                                    保存
-                                </Button>
-                            </FormItem>
-                        </Form>
-                    </Spin>
-                </Card>
+                                    ) : (
+                                        <span style={{ marginLeft: 10 }}>未上传</span>
+                                    )}
+                                </Upload>
+                            )}
+                        </FormItem>
+                        <FormItem {...formItemLayout} label="回调域名" extra="使用场景：微信支付异步通知、退款原路返回"
+                        >
+                            {getFieldDecorator("callback_domain", {
+                                rules: [
+                                    {
+                                        required: true,
+                                        message: "请输入回调域名"
+                                    }
+                                ],
+                                initialValue: callback_domain
+                            })(<Input placeholder="请输入回调域名，如：https://www.fashop.cn" />)}
+                        </FormItem>
+                        <FormItem {...formItemLayout} label="支付开关">
+                            {getFieldDecorator("status", {
+                                valuePropName: "checked",
+                                initialValue: status === 1
+                            })(<Switch />)}
+                        </FormItem>
+                        <FormItem {...tailFormItemLayout}>
+                            <Button type="primary" htmlType="submit" loading={settingEditLoading}>
+                                保存
+                            </Button>
+                        </FormItem>
+                    </Form>
+                </Spin>
+            </Card>
         );
     }
 
