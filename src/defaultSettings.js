33b514cfc97b2f457dfbfd3868f48f6a45349815
韩文博 <9476400@qq.com>
2019-01-22 22:19:47 +0800

    feat: add metaTitle ，remove useless files

--- src/defaultSettings.js
+++ src/defaultSettings.js
@@ -1,11 +1,14 @@
 module.exports = {
-    navTheme: "dark", // theme for nav menu
-    primaryColor: "#1890FF", // primary color of ant design
-    layout: "sidemenu", // nav menu position: sidemenu or topmenu
-    contentWidth: "Fluid", // layout of content: Fluid or Fixed, only works when layout is topmenu
-    fixedHeader: false, // sticky header
-    enableMenuLocale: false,
-    autoHideHeader: false, // auto hide header
-    fixSiderbar: true, // sticky siderbar
-    title: 'FaShop',
+  navTheme: "dark", // theme for nav menu
+  primaryColor: "#1890FF", // primary color of ant design
+  layout: "sidemenu", // nav menu position: sidemenu or topmenu
+  contentWidth: "Fluid", // layout of content: Fluid or Fixed, only works when layout is topmenu
+  fixedHeader: false, // sticky header
+  autoHideHeader: false, // auto hide header
+  fixSiderbar: true, // sticky siderbar
+  menu: {
+    disableLocal: false
+  },
+  title: "FaShop",
+  metaTitle:"FaShop 商城系统 - Power By FaShop ( www.fashop.cn )"
 };
