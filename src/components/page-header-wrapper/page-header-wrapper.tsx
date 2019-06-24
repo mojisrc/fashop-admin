3c8356db19d98a52e11af33944d874f7468124e0
wangxingkang <wangxingkang@sensoro.com>
2019-06-25 00:56:59 +0800

    fix: 修复动态路由面包屑不显示

--- src/components/page-header-wrapper/page-header-wrapper.tsx
+++ src/components/page-header-wrapper/page-header-wrapper.tsx
@@ -3,6 +3,7 @@ import Link from 'umi/link';
 import classNames from 'classnames';
 import { PageHeader } from 'antd';
 import { PageHeaderProps } from 'antd/es/page-header';
+import pathToRegexp from 'path-to-regexp';
 import MenuContext from '@/layouts/menu-context';
 import { urlToList } from 'awe-utils';
 import './page-header-wrapper.less';
@@ -20,17 +21,27 @@ const PageHeaderWrapper: React.FC<IProps> = props => {
     const routes = [];
 
     pathSnippets.forEach(url => {
-      const menuData = breadcrumbNameMap[url];
-      routes.push({
-        path: menuData.path,
-        breadcrumbName: menuData.name,
+      const list = Object.keys(breadcrumbNameMap).filter(item => {
+        return pathToRegexp(item).test(url);
       });
+      if (list && list.length) {
+        const menuData = breadcrumbNameMap[list[0]];
+        if (menuData) {
+          routes.push({
+            path: menuData.path,
+            breadcrumbName: menuData.name,
+          });
+        }
+      }
     });
 
     return routes;
   };
 
-  const itemRender = (route, params, routes) => {
+  const pathname = location!.pathname;
+  const routes = getRoutes(pathname, breadcrumbNameMap);
+
+  const handleItemRender = (route, params, routes, paths) => {
     const last = routes.indexOf(route) === routes.length - 1;
     return last ? (
       <span>{route.breadcrumbName}</span>
@@ -38,9 +49,6 @@ const PageHeaderWrapper: React.FC<IProps> = props => {
       <Link to={route.path}>{route.breadcrumbName}</Link>
     );
   };
-
-  const pathname = location!.pathname;
-  const routes = getRoutes(pathname, breadcrumbNameMap);
 
   return (
     <div
@@ -50,7 +58,7 @@ const PageHeaderWrapper: React.FC<IProps> = props => {
     >
       <PageHeader
         breadcrumb={{
-          itemRender,
+          itemRender: handleItemRender,
           routes,
         }}
         {...restProps}
